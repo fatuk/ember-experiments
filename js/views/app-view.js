afa08333b00647db73984c421ad985c312d28f87
fatuk <fatukk@gmail.com>
2014-02-27 23:26:18 +0400

    Fix svg files paths

--- js/views/app-view.js
+++ js/views/app-view.js
@@ -5,12 +5,12 @@ App.ApplicationView = Ember.View.extend({
 		var draw = SVG('frontPreview'),
 			view = this;
 
-		$.get('../svg/form.txt', function(data) {
+		$.get('svg/form.txt', function(data) {
 			var svg = draw.svg(data);
 			App.bagForm = svg.get('SvgjsPath1007').fill('#ccc').stroke({color: '#979797', width: 2});
 		});
 
-		$.get('../svg/upPocket1.txt', function(data) {
+		$.get('svg/upPocket1.txt', function(data) {
 			var svg = draw.svg(data);
 			App.upPocket = svg.get('SvgjsPath1009').
 				fill('#ccc').
